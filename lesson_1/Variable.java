38b0bc11b58509dcc1259bc514938088216f479c
SadSparrow <77299989+SadSparrow@users.noreply.github.com>
2021-01-12 20:23:04 +0300

    Update Variable.java

--- lesson_1/Variable.java
+++ lesson_1/Variable.java
@@ -1,12 +1,12 @@
 public class Variable {
     public static void main(String[] args) {
-        byte memory = 8; //RAM
-        short memoryV = 15;
-        int versionS = 3;
-        long proc = 2304L;
+        byte ram = 8; 
+        short memoryVirtual = 15;
+        int versionSMBIOS = 3;
+        long cpu = 2304L;
 
         float versionMainBoard = 1.19f;
-        double versionK = 1.19;
+        double versionController = 1.19;
 
         char disk = 'C';
         boolean b = true;
