36798f491df6f5db76c8692ea3217db10537ac1f
unknown <ulmoniss@yandex.ru>
2021-01-14 22:51:21 +0300

    implement hw_2

---
+++ lesson_2/Wolf.java
@@ -0,0 +1,27 @@
+public class Wolf {
+    String gender;
+    String nickname;
+    int weight;
+    int age;
+    String color;
+
+    public void walk() {
+        System.out.println("walk");
+    }
+
+    public void sit() {
+        System.out.println("sit");
+    }
+
+    public void run() {
+        System.out.println("run");
+    }
+
+    public void howl() {
+        System.out.println("howl");
+    }
+
+    public void hunt() {
+        System.out.println("hunt");
+    }
+}
