09d7bb28162c5f30f0f6bcf59eab25ff04dac37c
unknown <ulmoniss@yandex.ru>
2021-01-18 22:28:35 +0300

    attempt_4

--- lesson_2/GuessNumber.java
+++ lesson_2/GuessNumber.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class GuessNumber {
     private Player playerFirst;
     private Player playerSecond;
-    int quizNumber;
+    private int quizNumber;
     Scanner scan = new Scanner(System.in);
 
     public GuessNumber(Player playerFirst, Player playerSecond) {
@@ -19,27 +19,27 @@ public class GuessNumber {
         while (true) {
             System.out.print("Очередь игрока " + playerFirst.getName() + ": ");
             playerFirst.setNumber(scan.nextInt());
-            if (guessNumber(playerFirst, quizNumber)) {
+            if (guessNumber(playerFirst)) {
                 break;
             }
             System.out.print("Очередь игрока " + playerSecond.getName() + ": ");
             playerSecond.setNumber(scan.nextInt());
-            if (guessNumber(playerSecond, quizNumber)) {
+            if (guessNumber(playerSecond)) {
                 break;
             }
         }
     }
 
-    private boolean guessNumber(Player p, int quizNumber) {
+    private boolean guessNumber(Player p) {
         if (quizNumber < p.getNumber()) {
             System.out.println("Введенное игроком " + p.getName() + " число больше того, что загадал компьютер");
-            return false;
-        } else if (quizNumber > p.getNumber()) {
+        }
+        if (quizNumber > p.getNumber()) {
             System.out.println("Введенное игроком " + p.getName() + " число меньше того, что загадал компьютер");
-            return false;
-        } else {
+        }
+        if (quizNumber == p.getNumber()) {
             System.out.println("Игрок " + p.getName() + " угадал число!");
         }
-        return true;
+        return quizNumber == p.getNumber();
     }
 }
