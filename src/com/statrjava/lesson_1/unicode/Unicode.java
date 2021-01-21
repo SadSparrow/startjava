71990283bd2d7aca0a5d0d6bdddf02aff4cb357a
unknown <ulmoniss@yandex.ru>
2021-01-21 22:16:38 +0300

    char

--- src/com/statrjava/lesson_1/unicode/Unicode.java
+++ src/com/statrjava/lesson_1/unicode/Unicode.java
@@ -2,8 +2,8 @@ package com.statrjava.lesson_1.unicode;
 
 public class Unicode {
     public static void main(String[] args) {
-        for (int i = 33; i <= 126; i++) {
-            System.out.println((char) i);
+        for (char c = 33; c <= 126; c++) {
+            System.out.print(c);
         }
     }
 }
