d027bb07be3ce6c177763eb753d63b7982a9f4dd
unknown <ulmoniss@yandex.ru>
2021-01-23 15:27:05 +0300

    game with array

---
+++ src/com/statrjava/lesson_4/game/GuessNumberTest.java
@@ -0,0 +1,25 @@
+package com.statrjava.lesson_4.game;
+
+import java.util.Scanner;
+
+public class GuessNumberTest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String playerAnswer = "да";
+
+        System.out.print("Введите имя игрока №1: ");
+        Player firstPlayer = new Player(scan.nextLine());
+        System.out.print("Введите имя игрока №2: ");
+        Player secondPlayer = new Player(scan.nextLine());
+        GuessNumber game = new GuessNumber(firstPlayer, secondPlayer);
+        System.out.println("У вас " + game.countOfTry + " попыток");
+
+        while (playerAnswer.equals("да")) {
+            game.start();
+            do {
+                System.out.print("Хотите продолжить игру? [да/нет]: ");
+                playerAnswer = scan.nextLine();
+            } while (!playerAnswer.equals("да") && !playerAnswer.equals("нет"));
+        }
+    }
+}
---
+++ src/com/statrjava/lesson_4/game/Player.java
@@ -0,0 +1,45 @@
+package com.statrjava.lesson_4.game;
+
+import java.util.Arrays;
+
+public class Player {
+    private final String name;
+    private final int[] numbers = new int[10];
+    private int index = 0;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public int getNumber() {
+        return numbers[index - 1];
+    }
+
+    public void setNumber(int number) {
+        numbers[index] = number;
+        index++;
+    }
+
+    public void getNumbersToString() {
+        StringBuilder sb = new StringBuilder();
+        int[] numbers = Arrays.copyOf(this.numbers, index);
+        for (int i : numbers) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+
+    public void cleanArray() {
+        //int[] numbers = Arrays.copyOf(this.numbers, index);
+        Arrays.fill(numbers, 0);
+        index = 0;
+    }
+}
